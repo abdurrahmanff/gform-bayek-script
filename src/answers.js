634b0b7531dc7be3e3273cc41bd71a67bd918dcb
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-07-30 07:36:46 +0700

    init

---
+++ src/answers.js
@@ -0,0 +1,49 @@
+const answers = [
+  {
+    Hari: 'Senin (Shift Pagi)', // Sesuaiin sama yang mau dipilih (ini required)
+    Tanggal: '2023-08-01', // Format YYYY-MM-DD (ini required)
+    'Nama Operator 1/Angkatan (Bulan Tahun)': 'Bayek/Januari 2019', // (ini required)
+    'Nama Operator 2/Angkatan (Bulan Tahun)': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+    'Nama Pasien/Usia/no RM': 'Gembol', // (ini required)
+    'Jenis Tindakan Operator 1': 'RM Baru', //
+    'Jenis Tindakan Operator 2': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+  },
+  {
+    Hari: 'Selasa (Shift Siang)', // Sesuaiin sama yang mau dipilih (ini required)
+    Tanggal: '2023-08-01', // Format YYYY-MM-DD (ini required)
+    'Nama Operator 1/Angkatan (Bulan Tahun)': 'Bayek/Januari 2019', // (ini required)
+    'Nama Operator 2/Angkatan (Bulan Tahun)': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+    'Nama Pasien/Usia/no RM': 'Gembol', // (ini required)
+    'Jenis Tindakan Operator 1': 'RM Baru', //
+    'Jenis Tindakan Operator 2': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+  },
+  {
+    Hari: 'Rabu (Shift Pagi)', // Sesuaiin sama yang mau dipilih (ini required)
+    Tanggal: '2023-08-01', // Format YYYY-MM-DD (ini required)
+    'Nama Operator 1/Angkatan (Bulan Tahun)': 'Bayek/Januari 2019', // (ini required)
+    'Nama Operator 2/Angkatan (Bulan Tahun)': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+    'Nama Pasien/Usia/no RM': 'Gembol', // (ini required)
+    'Jenis Tindakan Operator 1': 'RM Baru', //
+    'Jenis Tindakan Operator 2': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+  },
+  {
+    Hari: 'Kamis (Shift Siang)', // Sesuaiin sama yang mau dipilih (ini required)
+    Tanggal: '2023-08-01', // Format YYYY-MM-DD (ini required)
+    'Nama Operator 1/Angkatan (Bulan Tahun)': 'Bayek/Januari 2019', // (ini required)
+    'Nama Operator 2/Angkatan (Bulan Tahun)': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+    'Nama Pasien/Usia/no RM': 'Gembol', // (ini required)
+    'Jenis Tindakan Operator 1': 'RM Baru', //
+    'Jenis Tindakan Operator 2': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+  },
+  {
+    Hari: 'Jumat (Shift Pagi)', // Sesuaiin sama yang mau dipilih (ini required)
+    Tanggal: '2023-08-01', // Format YYYY-MM-DD (ini required)
+    'Nama Operator 1/Angkatan (Bulan Tahun)': 'Bayek/Januari 2019', // (ini required)
+    'Nama Operator 2/Angkatan (Bulan Tahun)': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+    'Nama Pasien/Usia/no RM': 'Gembol', // (ini required)
+    'Jenis Tindakan Operator 1': 'RM Baru', //
+    'Jenis Tindakan Operator 2': '', // (ini ga, kalo ga pen ngisi kosongin aja)
+  },
+];
+
+module.exports = answers;
